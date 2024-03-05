121a9f5b3de44aebfdcb6533f782f0136edca541
MeltemTutar <meltemtutar12@gmail.com>
2024-03-04 22:25:22 -0500

    change

--- Respond/constants.py
+++ Respond/constants.py
@@ -1,28 +1,32 @@
 
 maf_file_path = 'input_data/RESPOND_247_coding_final.maf'
 expression_file_path = 'input_data/Expression_remove_BE.txt'
-output_folder='output_data_v2'
-genes = ['FOXA1',
-        'GYPB', 
-        'ID1', 
-        'RPL10', 
-        'SPOP', 
-        'TSPAN9',
-        'VPS33A', 
-        'NBPF1',
-        'ZMYM3', 
-        'COL17A1', 
-        'UBR4', 
-        'CHD3', 
-        'TRPM1', 
-        'NDST2', 
-        'CHD7', 
-        'ACACA', 
-        'TRRAP', 
-        'CSMD2', 
-        'RYR1', 
-        'HTT', 
-        'ERF', 
-        'MED12', 
-        'CHD6', 
-        'MYO15A']
+output_folder='output_data_NBPF1'
+# genes = ['FOXA1',
+#         'GYPB', 
+#         'ID1', 
+#         'RPL10', 
+#         'SPOP', 
+#         'TSPAN9',
+#         'VPS33A', 
+#         'NBPF1',
+#         'ZMYM3', 
+#         'COL17A1', 
+#         'UBR4', 
+#         'CHD3', 
+#         'TRPM1', 
+#         'NDST2', 
+#         'CHD7', 
+#         'ACACA', 
+#         'TRRAP', 
+#         'CSMD2', 
+#         'RYR1', 
+#         'HTT', 
+#         'ERF', 
+#         'MED12', 
+#         'CHD6', 
+#         'MYO15A']
+
+# genes = ['TRPM1', 'ERF', 'MED12', 'CHD6', 'TSPAN9']
+
+genes = ['NBPF1']
